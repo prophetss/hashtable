82c2c66df35d252ccdeb29f110f3825afc8e5fce
Prophetss <w_haoliang@126.com>
2019-08-30 01:03:08 +0800

    improve

--- hashtable.c
+++ hashtable.c
@@ -17,66 +17,86 @@
 /*64位使用xx64，32位使用xx32*/
 #if defined(__x86_64__) || defined(_WIN64)
 #define XXHASH(a, b, c)     XXH64(a, b, c)
-#define MAX_COUNT	0xffffffffffffffffUL
+#define XXH_hash_t	XXH64_hash_t
 #else
 #define XXHASH(a, b, c)     XXH32(a, b, c)
-#define MAX_COUNT	0xffffffffUL
+#define XXH_hash_t	XXH32_hash_t
 #endif
 
-#define INDEX(hash, cap) ((hash) & ((cap)-1))
+#define MAX_COUNT	0x3fffffffU
+
+/*
+* 最大负载因子百分比，乘以表容量为实际最大可容纳元素数量值,
+* 超过此值表扩容
+*/
+#define MAX_LOAD_FACTOR              (75)
+
+/*
+* 最小负载因子百分比，表内元素数量少于此百分比,表缩减
+*/
+#define MIN_LOAD_FACTOR              (15)
+
+#define INDEX(hash, cap) (((U32)hash) & (((U32)cap)-1))
 
 /*默认key比较*/
-static inline int key_cmp(const void *key1, size_t key1_len, const void *key2, size_t key2_len)
+static inline int key_cmp(const void *key1, U32 key1_len, const void *key2, U32 key2_len)
 {
 	return ((key1_len == key2_len) && !memcmp(key1, key2, key1_len));
 }
 
-static unsigned char get_digits(hash_size_t n)
+//向上取2的幂(http://graphics.stanford.edu/~seander/bithacks.html#OperationCounting)
+static inline U32 ROUND_UP(U32 v)
 {
-	unsigned char count = 0;
-	do {
-		count++;
-	} while (n  >>= 1);
-	return count;
+	v--;
+	v |= v >> 1;
+	v |= v >> 2;
+	v |= v >> 4;
+	v |= v >> 8;
+	v |= v >> 16;
+	return ++v;
 }
 
-static hash_table_element_t* element_new(hash_table_t *table, const void *key, size_t key_len, void *value, size_t value_len)
+static void* ht_alloc(size_t len)
 {
-	hash_table_element_t *element = (hash_table_element_t*)malloc(sizeof(hash_table_element_t));
-	if (!element) sys_exit_throw();
+	void* data = malloc(len);
+	if (!data) sys_exit_throw();
+	return data;
+}
 
-	element->key = malloc(key_len);
-	if (!element->key) sys_exit_throw();
+static void* ht_calloc(size_t len, size_t size)
+{
+	void* data = calloc(len, size);
+	if (!data) sys_exit_throw();
+	return data;
+}
+
+static hash_table_element_t* element_new(hash_table_t *table, const void *key, U32 key_len, void *value, U32 value_len)
+{
+	hash_table_element_t* element = (hash_table_element_t*)ht_alloc(sizeof(hash_table_element_t));
+	element->key = ht_alloc(key_len);
 	memcpy(element->key, key, key_len);
-
 	if (table->mode == COPY_MODE) {
-		element->value = malloc(value_len);
-		if (!element->value) sys_exit_throw();
+		element->value = ht_alloc(value_len);
 		memcpy(element->value, value, value_len);
 	}
-	else {
-		element->value = value;
-	}
-	element->key_len = key_len;
+	else element->value = value;
 	element->value_len = value_len;
+	element->key_len = key_len;
 	element->next = NULL;
-
 	return element;
 }
 
 static void element_delete(hash_table_t *table, hash_table_element_t *element)
 {
-	if (table->mode == COPY_MODE) {
-		free(element->value);
-	}
+	if (table->mode == COPY_MODE) free(element->value);
 	free(element->key);
 	free(element);
 }
 
-static void data_store_delete(hash_table_t *table, hash_table_element_t **data_store, const hash_size_t capacity)
+static void data_store_delete(hash_table_t *table, hash_table_element_t **data_store, U32 capacity)
 {
 	if (!data_store) return;
-	for (hash_size_t i = 0; i < capacity; i++) {
+	for (U32 i = 0; i < capacity; i++) {
 		while (NULL != data_store[i]) {
 			hash_table_element_t *temp = data_store[i];
 			data_store[i] = data_store[i]->next;
@@ -85,33 +105,34 @@ static void data_store_delete(hash_table_t *table, hash_table_element_t **data_s
 	}
 }
 
-hash_table_t* hash_table_new_n(hash_size_t n, table_mode_t mode)
+hash_table_t* hash_table_new_n(U32 n, table_mode_t mode)
 {
-	hash_table_t *new_hashtable = (hash_table_t*)malloc(sizeof(hash_table_t));
-	if (!new_hashtable) sys_exit_throw();
-	unsigned char digits = get_digits(n);
-	new_hashtable->table_capacity_1 = (hash_size_t)(1 << digits);
-	new_hashtable->first_data_store = (hash_table_element_t**)calloc(new_hashtable->table_capacity_1, sizeof(hash_table_element_t*));
-	if (!new_hashtable->first_data_store) sys_exit_throw();
+	hash_table_t *new_hashtable = (hash_table_t*)ht_alloc(sizeof(hash_table_t));
+	new_hashtable->table_capacity_1 = ROUND_UP(n);
+	if (new_hashtable->table_capacity_1 > MAX_COUNT) {
+		new_hashtable->table_capacity_1 >>= 2;
+		fprintf(stderr, "Capacity Overflow! New Capacity(%u)\n", new_hashtable->table_capacity_1);
+	}
+	new_hashtable->first_data_store = (hash_table_element_t * *)ht_calloc(new_hashtable->table_capacity_1, sizeof(hash_table_element_t*));
 	new_hashtable->second_data_store = NULL;
 	new_hashtable->table_capacity_2 = 0;
 	new_hashtable->key_compare = key_cmp;
 	new_hashtable->rehashidx = 0;
 	new_hashtable->key_count = 0;
 	new_hashtable->mode = mode;
-	new_hashtable->seed = (hash_size_t)time(0);
+	new_hashtable->seed = (U32)time(0);
 	return new_hashtable;
 }
 
 void hash_table_destory(hash_table_t *table)
 {
-	data_store_delete(table, &(table->first_data_store[table->rehashidx]), table->table_capacity_1 - table->rehashidx);
+	data_store_delete(table, &table->first_data_store[table->rehashidx], table->table_capacity_1 - table->rehashidx);
 	data_store_delete(table, table->second_data_store, table->table_capacity_2);
 	free(table);
 }
 
 /*将first表内的count个元素转移至second表*/
-static int move_elements(hash_table_t *table, hash_size_t count)
+static int move_elements(hash_table_t *table, U32 count)
 {
 	hash_table_element_t *ftemp;
 	while (table->rehashidx < table->table_capacity_1) {
@@ -120,7 +141,7 @@ static int move_elements(hash_table_t *table, hash_size_t count)
 			continue; 
 		}
 		/*重新hash，插入链首*/
-		hash_size_t rehash_key = INDEX(XXHASH(ftemp->key, ftemp->key_len, table->seed), table->table_capacity_2);
+		U32 rehash_key = INDEX(XXHASH(ftemp->key, ftemp->key_len, table->seed), table->table_capacity_2);
 		hash_table_element_t *stemp = table->second_data_store[rehash_key];
 		table->second_data_store[rehash_key] = table->first_data_store[table->rehashidx];
 		table->first_data_store[table->rehashidx] = ftemp->next;
@@ -138,26 +159,26 @@ static int move_elements(hash_table_t *table, hash_size_t count)
 }
 
 /*扩大缩小只重新建表，不进行元素转移*/
-static void hash_table_expand(hash_table_t *table, hash_size_t new_size)
+static void hash_table_expand(hash_table_t *table, U32 new_size)
 {
-	table->second_data_store = (hash_table_element_t**)calloc(new_size, sizeof(hash_table_element_t*));
-	if (!table->second_data_store)  sys_exit_throw();
+	if (new_size > MAX_COUNT || new_size < DEFAULT_CAPACITY) return;
+	table->second_data_store = (hash_table_element_t**)ht_calloc(new_size, sizeof(void*));
 	table->table_capacity_2 = new_size;
 }
 
-static void get_tables(hash_table_t* table, const void* key, size_t key_len, hash_table_element_t** ht, hash_size_t* h)
+static void get_tables(hash_table_t* table, const void* key, U32 key_len, hash_table_element_t** ht, U32* h)
 {
-	hash_size_t hash = XXHASH(key, key_len, table->seed);
+	XXH_hash_t hash = XXHASH(key, key_len, table->seed);
 	h[1] = (table->table_capacity_2 == 0 ? 0 : INDEX(hash, table->table_capacity_2));
 	ht[1] = (NULL != table->second_data_store && 0 == move_elements(table,2) ? table->second_data_store[h[1]] : NULL);
 	h[0] = INDEX(hash, table->table_capacity_1);
 	ht[0] = table->first_data_store[h[0]];
 }
 
-void* hash_table_lookup(hash_table_t *table, const void *key, size_t key_len)
+void* hash_table_lookup(hash_table_t *table, const void *key, U32 key_len)
 {
 	hash_table_element_t* temp[2] = { 0 };
-	hash_size_t hash_key[2] = { 0 };
+	U32 hash_key[2] = { 0 };
 	get_tables(table, key, key_len, temp, hash_key);
 	for (int i = 0; i < 2; ++i) {
 		while (temp[i]) {
@@ -172,28 +193,25 @@ void* hash_table_lookup(hash_table_t *table, const void *key, size_t key_len)
 
 static void _expand_if_needed(hash_table_t *table)
 {
-	if (!table->second_data_store && table->key_count*100/MAX_LOAD_FACTOR > table->table_capacity_1)
+	if (!table->second_data_store && table->key_count/MAX_LOAD_FACTOR * 100 > table->table_capacity_1)
 		hash_table_expand(table, table->table_capacity_1*2);
 }
 
-static int replace_element(void** val, void* new_val, size_t len, table_mode_t mode)
+static void replace_element(void** val, void* new_val, size_t len, table_mode_t mode)
 {
-	if (mode == REF_MODE) {
-		*val = new_val;
-		return 0;
+	if (mode == COPY_MODE) {
+		free(*val);
+		*val = ht_alloc(len);
+		memcpy(*val, new_val, len);
 	}
-	free(*val);
-	*val = malloc(len);
-	if (!*val) sys_exit_throw();
-	memcpy(*val, new_val, len);
-	return 0;
+	else *val = new_val;
 }
 
-int hash_table_set(hash_table_t *table, const void *key, size_t key_len, void *value, size_t value_len, int mode)
+int hash_table_set(hash_table_t *table, const void *key, U32 key_len, void *value, U32 value_len, set_mode_t mode)
 {
 	_expand_if_needed(table);
 	hash_table_element_t *prev = NULL, *temp[2] = { 0 };
-	hash_size_t hash_key[2] = { 0 };
+	U32 hash_key[2] = { 0 };
 	get_tables(table, key, key_len, temp, hash_key);
 	/*检索key发现相同替换返回1，否则加入链表末尾count+1返回0*/
 	for (int i = 0; i < 2; ++i) {
@@ -224,14 +242,14 @@ int hash_table_set(hash_table_t *table, const void *key, size_t key_len, void *v
 
 static void _resize_if_needed(hash_table_t *table)
 {
-	if (!table->second_data_store && table->key_count > DEFAULT_CAPACITY && (table->key_count*100/MIN_LOAD_FACTOR < table->table_capacity_1))
+	if (!table->second_data_store && table->key_count > DEFAULT_CAPACITY && (table->key_count/MIN_LOAD_FACTOR * 100 < table->table_capacity_1))
 		hash_table_expand(table, table->table_capacity_1/2);
 }
 
-int hash_table_remove(hash_table_t *table, const void *key, size_t key_len)
+int hash_table_remove(hash_table_t *table, const void *key, U32 key_len)
 {
 	hash_table_element_t *temp[2] = { NULL }, **ptemp[2] = { NULL };
-	hash_size_t hash_key[2] = { 0 };
+	U32 hash_key[2] = { 0 };
 	get_tables(table, key, key_len, temp, hash_key);
 	if (temp[1]) ptemp[1] = &(table->second_data_store[hash_key[1]]);
 	if (temp[0]) ptemp[0] = &(table->first_data_store[hash_key[0]]);
@@ -254,18 +272,16 @@ int hash_table_remove(hash_table_t *table, const void *key, size_t key_len)
 	return -1;
 }
 
-static void get_elements(hash_table_element_t **p, hash_table_element_t **data_store, hash_size_t n, table_mode_t mode)
+static void get_elements(hash_table_element_t **p, hash_table_element_t **data_store, U32 n, table_mode_t mode)
 {
-	for (hash_size_t i = 0; i < n; ++i) {
+	for (U32 i = 0; i < n; ++i) {
 		hash_table_element_t *tmp = data_store[i];
 		while (tmp) {
 			memcpy(*p, tmp, sizeof(hash_table_element_t));
+			(*p)->key = ht_alloc(tmp->key_len);
+			memcpy((*p)->key, tmp->key, tmp->key_len);
 			if (mode == COPY_MODE) {
-				(*p)->key = malloc(tmp->key_len);
-				if (!(*p)->key) sys_exit_throw();
-				memcpy((*p)->key, tmp->key, tmp->key_len);
-				(*p)->value = malloc(tmp->value_len);
-				if (!(*p)->value) sys_exit_throw();
+				(*p)->value = ht_alloc(tmp->value_len);
 				memcpy((*p)->value, tmp->value, tmp->value_len);
 			}
 			tmp = tmp->next;
@@ -280,8 +296,8 @@ hash_table_element_t* hash_table_elements(hash_table_t *table, table_mode_t mode
 	if (0 == table->key_count) return NULL;
 
 	/*创建返回列表*/
-	hash_table_element_t *head = (hash_table_element_t*)calloc(table->key_count, sizeof(hash_table_element_t));
-	if (!head) sys_exit_throw();
+	hash_table_element_t *head = (hash_table_element_t*)ht_calloc(table->key_count, sizeof(hash_table_element_t));
+	
 
 	hash_table_element_t *ptmp = head;
 
@@ -294,7 +310,7 @@ hash_table_element_t* hash_table_elements(hash_table_t *table, table_mode_t mode
 	return head;
 }
 
-void hash_table_rehash(hash_table_t* table, hash_size_t seed)
+void hash_table_rehash(hash_table_t* table, U32 seed)
 {
 	if (table->key_count == 0) return;
 	table->seed = seed;
@@ -304,7 +320,7 @@ void hash_table_rehash(hash_table_t* table, hash_size_t seed)
 	}
 	else {
 		hash_table_element_t** p = table->first_data_store;
-		hash_size_t p_cap = table->table_capacity_1;
+		U32 p_cap = table->table_capacity_1;
 		table->first_data_store = table->second_data_store;
 		table->table_capacity_1 = table->table_capacity_2;
 		hash_table_expand(table, table->table_capacity_2);
@@ -316,7 +332,7 @@ void hash_table_rehash(hash_table_t* table, hash_size_t seed)
 	}
 }
 
-void ht_set_compare_func(hash_table_t* table, int (*kc_func)(const void*, size_t, const void*, size_t))
+void ht_set_compare_func(hash_table_t* table, int (*kc_func)(const void*, U32, const void*, U32))
 {
 	table->key_compare = kc_func;
 }
--- hashtable.h
+++ hashtable.h
@@ -10,46 +10,24 @@ extern "C" {
 #include "xxhash.h"
 
 
-/*64位使用xx64，32位使用xx32*/
-#if defined(__x86_64__) || defined(_WIN64)
-#define hash_size_t         XXH64_hash_t
+#if !defined (__VMS) \
+  && (defined (__cplusplus) \
+  || (defined (__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L) /* C99 */) )
+#   include <stdint.h>
+	typedef uint32_t U32;
 #else
-#define hash_size_t         XXH32_hash_t
+	typedef unsigned int U32;
 #endif
-
-/*默认初始表容量*/
-#define DEFAULT_CAPACITY        (8)
-
-/*
-* 最大负载因子百分比，乘以表容量为实际最大可容纳元素数量值,
-* 超过此值表扩容
-*/
-#define MAX_LOAD_FACTOR              (75)
-
-/*
-* 最小负载因子百分比，表内元素数量少于此百分比,表缩减
-*/
-#define MIN_LOAD_FACTOR              (15)
-
-/*
-* 表内添加value时的方式：引用模式value传入的方式为引用，
-* 删除时不会释放；拷贝模式内部申请内存拷贝value值，删除
-* 会释放表内value
-*/
-typedef enum { REF_MODE = 0, COPY_MODE } table_mode_t;
-
-/*插入模式，模式1：相同key替换返回1，模式2：不替换返回-1*/
-typedef enum { MODE_1 = 0, MODE_2} set_mode_t;
 
 typedef struct hash_table_element hash_table_element_t;
 
 typedef struct hash_table_element
 {
 	/* key长度 */
-	size_t key_len;
+	U32 key_len;
 
 	/* value长度 */
-	size_t value_len;
+	U32 value_len;
 
 	/* key数据 */
 	void *key;
@@ -62,10 +40,20 @@ typedef struct hash_table_element
 
 } hash_table_element_t;
 
+/*
+* 表内添加value时的方式：引用模式传入的方式为指针拷贝，删除时不会释放；
+* 拷贝模式内部申请内存拷贝其值，删除会自己释放
+*/
+typedef enum 
+{ 
+	REF_MODE = 0,
+	COPY_MODE
+} table_mode_t;
+
 typedef struct hash_table
 {
 	/* 比较函数， 默认为memcmp */
-	int (*key_compare)(const void *key1, size_t key1_len, const void *key2, size_t key2_len);
+	int (*key_compare)(const void *key1, U32 key1_len, const void *key2, U32 key2_len);
 		
 	/*
 	* 初始表，希表分两个表以追求resize时负载均衡，当表扩容时会逐
@@ -74,46 +62,48 @@ typedef struct hash_table
 	*/
 	hash_table_element_t  **first_data_store;
 
-	/* first表容量 */
-	hash_size_t table_capacity_1;
-
 	/*
 	* 初始为NULL，发生表扩容时产生，大小为first表两倍，此后
 	* 在插入查询删除时会逐个将first表中元素移至second表中
 	*/
 	hash_table_element_t  **second_data_store;
 
+	/* first表容量 */
+	U32 table_capacity_1;
+
 	/* second表容量 */
-	hash_size_t table_capacity_2;
+	U32 table_capacity_2;
 
 	/* 当前表内key数量 */
-	hash_size_t key_count;
+	U32 key_count;
 
 	/*
 	* 记录指向first表中按顺序第一个key不为空的序号，扩容后
 	* first表元素移至second表时使用
 	*/
-	hash_size_t rehashidx;
-
-	/* 表内添加value时的方式：REF_MODE-为引用模式，COPY_MODE-为拷贝模式 */
-	table_mode_t mode;
+	U32 rehashidx;
 
 	/* hash种子，默认为time(0) */
-	hash_size_t seed;
+	U32 seed;
+
+	/* 表内添加value时的方式(拷贝/引用) */
+	table_mode_t mode;
 
 } hash_table_t;
 
+/*插入模式，模式1：相同key替换返回1，模式2：不替换返回-1*/
+typedef enum { MODE_1 = 0, MODE_2 } set_mode_t;
 
 /*表创建，n-表容量，mode-模式，表容量实际会向上扩至2的次幂*/
-hash_table_t* hash_table_new_n(hash_size_t n, table_mode_t mode);
+hash_table_t* hash_table_new_n(U32 n, table_mode_t mode);
 
-int hash_table_set(hash_table_t* table, const void* key, size_t key_len, void* value, size_t value_len, int mode);
+int hash_table_set(hash_table_t* table, const void* key, U32 key_len, void* value, U32 value_len, set_mode_t mode);
 
 /*查询，成功返回value指针，失败返回NULL*/
-void* hash_table_lookup(hash_table_t* table, const void* key, size_t key_len);
+void* hash_table_lookup(hash_table_t* table, const void* key, U32 key_len);
 
 /*删除，失败返回-1，成功返回0*/
-int hash_table_remove(hash_table_t* table, const void* key, size_t key_len);
+int hash_table_remove(hash_table_t* table, const void* key, U32 key_len);
 
 /*删除表释放资源*/
 void hash_table_destory(hash_table_t* table);
@@ -125,9 +115,12 @@ void hash_table_destory(hash_table_t* table);
 hash_table_element_t* hash_table_elements(hash_table_t* table, table_mode_t mode);
 
 /*以新hash种子重新映射表内元素*/
-void hash_table_rehash(hash_table_t* table, hash_size_t seed);
+void hash_table_rehash(hash_table_t* table, U32 seed);
 
-#define htnew(mode)    hash_table_new_n(DEFAULT_CAPACITY, mode)
+/*默认初始表容量*/
+#define DEFAULT_CAPACITY        (8)
+
+#define htnew()    hash_table_new_n(DEFAULT_CAPACITY, REF_MODE)
 
 /*添加，成功返回0，key相同替换返回1*/
 #define htset(table, key, key_len, value, value_len)	hash_table_set(table, key, key_len, value, value_len, MODE_1)
@@ -146,7 +139,7 @@ void hash_table_rehash(hash_table_t* table, hash_size_t seed);
 #define htrh(table, seed)	hash_table_rehash(table, seed)
 
 /*设置键值比较函数(对应参数：key1, key1_len, key2, key2_len。返回值：不相同0，其他)。如无设置默认使用memcmp*/
-void ht_set_compare_func(hash_table_t* table, int (*kc_func)(const void*, size_t, const void*, size_t));
+void ht_set_compare_func(hash_table_t* table, int (*kc_func)(const void*, U32, const void*, U32));
 
 
 #if defined (__cplusplus)
